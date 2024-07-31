6d11f95a6dc97ea30bc26f896d8012a912d4ff6f
sohee <s2h110@naver.com>
2024-07-31 09:59:29 +0900

    refactor : response 객체에서 data를 object destructuring

--- Sprint4/ProductService.js
+++ Sprint4/ProductService.js
@@ -5,8 +5,7 @@ const baseUrl = "https://sprint-mission-api.vercel.app/products";
 export async function getProductList(page, pageSize, keyword) {
     const URL = `${baseUrl}?page=${page}&pageSize=${pageSize}&keyword=${keyword}`;
     try {
-      const response = await axios.get(URL);
-      const data = response.data;
+      const { data } = await axios.get(URL);
       return console.log('상품 리스트를 가져왔습니다.',data);
     } catch (error) {
       console.error(`Error: ${error}`);
@@ -17,8 +16,7 @@ export async function getProductList(page, pageSize, keyword) {
 export async function getProduct(id) {
     const URL = `${baseUrl}/${id}`;
     try {
-        const response = await axios.get(URL);
-        const data = response.data;
+        const {data} = await axios.get(URL);
         return console.log('상품 정보를 가져왔습니다.',data);
     } catch (error) {
         console.error(`Error: ${error}`);
@@ -29,7 +27,7 @@ export async function getProduct(id) {
 export async function createProduct(product) {
 
     try {
-      const response = await axios.post(baseUrl,
+      const {data} = await axios.post(baseUrl,
         {
         name: product.name,
         description: product.description,
@@ -38,7 +36,6 @@ export async function createProduct(product) {
         images: product.images
         }
       );
-      const data = response.data;
       return console.log('새로운 상품이 추가되었습니다.', data);
     } catch (error) {
       console.error(`Error : ${error}`);
@@ -50,8 +47,7 @@ export async function patchProduct(id, Product) {
     const URL = `${baseUrl}/${id}`;
   
     try {
-      const response = await axios.patch(URL, Product);
-      const data = response.data;
+      const {data} = await axios.patch(URL, Product);
       return console.log('상품 업데이트가 완료되었습니다.',data);
     } catch (error) {
       console.error(`Error : ${error}`);
@@ -63,8 +59,7 @@ export async function deleteProduct(id) {
     const URL = `${baseUrl}/${id}`;
   
     try {
-      const response = await axios.delete(URL);
-      const data = response.data;
+      const {data} = await axios.delete(URL);
       return console.log('삭제가 완료되았습니다.',data);
     } catch (error) {
       console.error(`Error : ${error}`);
